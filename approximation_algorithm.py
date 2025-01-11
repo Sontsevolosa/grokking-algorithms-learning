556a3a1d00b10c127a3bc26d143b55384800e33a
Sontsevolosa <sontsevolosa@gmail.com>
2025-01-11 16:38:44 +0200

    Add an example of approximation algorithm

---
+++ approximation_algorithm.py
@@ -0,0 +1,31 @@
+# TODO: should improve this code
+
+states_needed = set(["mt", "wa", "or", "id", "nv", "ut", "ca", "az"])
+
+stations = {}
+stations["kone"] = set(["id", "nv", "ut"])
+stations["ktwo"] = set(["wa", "id", "mt"])
+stations["kthree"] = set(["or", "nv", "ca"])
+stations["kfour"] = set(["nv", "ut"])
+stations["kfive"] = set(["ca", "az"])
+
+
+def find_covering(states_needed, stations):
+  final_stations = set()
+
+  while states_needed:
+    best_station = None
+    states_covered = set()
+    for station, states_for_station in stations.items():
+      covered = states_needed & states_for_station
+      if len(covered) > len(states_covered):
+        best_station = station
+        states_covered = covered
+
+  states_needed -= states_covered
+  final_stations.add(best_station)
+  prind(final_stations)
+
+
+# how to use this code:
+find_covering(states_needed, stations)
